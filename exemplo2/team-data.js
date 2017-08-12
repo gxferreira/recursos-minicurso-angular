c3cee3c135137b73a3308516c5e9bbee8888fb04
Guilherme Xavier <gxferreira.crowdify@gmail.com>
2017-08-12 00:15:35 -0300

    recursos para o exemplo2

---
+++ exemplo2/team-data.js
@@ -0,0 +1,1104 @@
+function getTeamData(){
+	return [
+	{
+		"id": 262,
+		"nome": "Flamengo",
+		"abreviacao": "FLA",
+		"posicao": 5,
+		"escudos": {
+			"grande": "img/escudos/flamengo_60x60.png",
+			"medio": "img/escudos/flamengo_45x45.png",
+			"pequeno": "img/escudos/flamengo_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Paolo Guerrero",
+				"gols": 5
+			},
+			{
+				"nome": "Diego",
+				"gols": 4
+			},
+			{
+				"nome": "Everton",
+				"gols": 3
+			},
+			{
+				"nome": "Berrío ",
+				"gols": 2
+			},
+			{
+				"nome": "Réver",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Éverton Ribeiro",
+				"assistencias": 4
+			},
+			{
+				"nome": "Rodinei",
+				"assistencias": 3
+			},
+			{
+				"nome": "Everton",
+				"assistencias": 2
+			},
+			{
+				"nome": "Paolo Guerrero",
+				"assistencias": 2
+			},
+			{
+				"nome": "Diego",
+				"assistencias": 2
+			}
+		]
+	},
+	{
+		"id": 263,
+		"nome": "Botafogo",
+		"abreviacao": "BOT",
+		"posicao": 7,
+		"escudos": {
+			"grande": "img/escudos/botafogo_60x60.png",
+			"medio": "img/escudos/botafogo_45x45.png",
+			"pequeno": "img/escudos/botafogo_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Roger",
+				"gols": 6
+			},
+			{
+				"nome": "Bruno Silva",
+				"gols": 4
+			},
+			{
+				"nome": "Rodrigo Pimpão",
+				"gols": 2
+			},
+			{
+				"nome": "Marcos Vinícius",
+				"gols": 2
+			},
+			{
+				"nome": "Guilherme",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "João Paulo",
+				"assistencias": 3
+			},
+			{
+				"nome": "Rodrigo Pimpão",
+				"assistencias": 3
+			},
+			{
+				"nome": "Bruno Silva",
+				"assistencias": 2
+			},
+			{
+				"nome": "Roger",
+				"assistencias": 2
+			},
+			{
+				"nome": "Gilson",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 264,
+		"nome": "Corinthians",
+		"abreviacao": "COR",
+		"posicao": 1,
+		"escudos": {
+			"grande": "img/escudos/corinthians_60x60.png",
+			"medio": "img/escudos/corinthians_45x45.png",
+			"pequeno": "img/escudos/corinthians_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Jô",
+				"gols": 11
+			},
+			{
+				"nome": "Jadson",
+				"gols": 4
+			},
+			{
+				"nome": "Rodriguinho",
+				"gols": 3
+			},
+			{
+				"nome": "Balbuena",
+				"gols": 3
+			},
+			{
+				"nome": "Clayson",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Maycon",
+				"assistencias": 3
+			},
+			{
+				"nome": "Rodriguinho",
+				"assistencias": 3
+			},
+			{
+				"nome": "Clayson",
+				"assistencias": 3
+			},
+			{
+				"nome": "Marquinhos Gabriel",
+				"assistencias": 3
+			},
+			{
+				"nome": "Jô",
+				"assistencias": 2
+			}
+		]
+	},
+	{
+		"id": 265,
+		"nome": "Bahia",
+		"abreviacao": "BAH",
+		"posicao": 14,
+		"escudos": {
+			"grande": "img/escudos/bahia_60x60.png",
+			"medio": "img/escudos/bahia_45x45.png",
+			"pequeno": "img/escudos/bahia_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Rodrigão",
+				"gols": 4
+			},
+			{
+				"nome": "Renê Júnior",
+				"gols": 3
+			},
+			{
+				"nome": "Régis",
+				"gols": 3
+			},
+			{
+				"nome": "Edigar Junio",
+				"gols": 2
+			},
+			{
+				"nome": "Juninho",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Allione",
+				"assistencias": 4
+			},
+			{
+				"nome": "Régis",
+				"assistencias": 4
+			},
+			{
+				"nome": "Zé Rafael ",
+				"assistencias": 3
+			},
+			{
+				"nome": "Juninho",
+				"assistencias": 2
+			},
+			{
+				"nome": "Lucas Fonseca",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 266,
+		"nome": "Fluminense",
+		"abreviacao": "FLU",
+		"posicao": 12,
+		"escudos": {
+			"grande": "img/escudos/fluminense_60x60.png",
+			"medio": "img/escudos/fluminense_45x45.png",
+			"pequeno": "img/escudos/Fluminense-escudo.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Henrique Dourado",
+				"gols": 10
+			},
+			{
+				"nome": "Wendel",
+				"gols": 3
+			},
+			{
+				"nome": "Wellington Silva",
+				"gols": 2
+			},
+			{
+				"nome": "Renato Chaves",
+				"gols": 1
+			},
+			{
+				"nome": "Marcos Junior",
+				"gols": 1
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Gustavo Scarpa",
+				"assistencias": 4
+			},
+			{
+				"nome": "Wendel",
+				"assistencias": 2
+			},
+			{
+				"nome": "Henrique Dourado",
+				"assistencias": 2
+			},
+			{
+				"nome": "Marcos Calazans",
+				"assistencias": 2
+			},
+			{
+				"nome": "Lucas",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 267,
+		"nome": "Vasco",
+		"abreviacao": "VAS",
+		"posicao": 8,
+		"escudos": {
+			"grande": "img/escudos/Vasco-65.png",
+			"medio": "img/escudos/Vasco-45.png",
+			"pequeno": "img/escudos/Vasco-30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Luis Fabiano",
+				"gols": 5
+			},
+			{
+				"nome": "Thalles",
+				"gols": 2
+			},
+			{
+				"nome": "Paulinho",
+				"gols": 2
+			},
+			{
+				"nome": "Nenê",
+				"gols": 2
+			},
+			{
+				"nome": "Yago Pikachu",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Nenê",
+				"assistencias": 3
+			},
+			{
+				"nome": "Guilherme Costa",
+				"assistencias": 2
+			},
+			{
+				"nome": "Luis Fabiano",
+				"assistencias": 2
+			},
+			{
+				"nome": "Mateus Vital",
+				"assistencias": 2
+			},
+			{
+				"nome": "Henrique",
+				"assistencias": 2
+			}
+		]
+	},
+	{
+		"id": 275,
+		"nome": "Palmeiras",
+		"abreviacao": "PAL",
+		"posicao": 4,
+		"escudos": {
+			"grande": "img/escudos/palmeiras_60x60.png",
+			"medio": "img/escudos/palmeiras_45x45.png",
+			"pequeno": "img/escudos/palmeiras_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Alejandro Guerra",
+				"gols": 4
+			},
+			{
+				"nome": "Róger Guedes",
+				"gols": 4
+			},
+			{
+				"nome": "Willian",
+				"gols": 3
+			},
+			{
+				"nome": "Keno",
+				"gols": 3
+			},
+			{
+				"nome": "Borja",
+				"gols": 3
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Alejandro Guerra",
+				"assistencias": 3
+			},
+			{
+				"nome": "Róger Guedes",
+				"assistencias": 3
+			},
+			{
+				"nome": "Tchê Tchê",
+				"assistencias": 2
+			},
+			{
+				"nome": "Zé Roberto",
+				"assistencias": 2
+			},
+			{
+				"nome": "Juninho",
+				"assistencias": 2
+			}
+		]
+	},
+	{
+		"id": 276,
+		"nome": "São Paulo",
+		"abreviacao": "SAO",
+		"posicao": 16,
+		"escudos": {
+			"grande": "img/escudos/sao_paulo_60x60.png",
+			"medio": "img/escudos/sao_paulo_45x45.png",
+			"pequeno": "img/escudos/sao_paulo_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Lucas Pratto",
+				"gols": 5
+			},
+			{
+				"nome": "Marcos Guilherme ",
+				"gols": 2
+			},
+			{
+				"nome": "Hernanes",
+				"gols": 2
+			},
+			{
+				"nome": "Marcinho",
+				"gols": 2
+			},
+			{
+				"nome": "Denilson",
+				"gols": 1
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Júnior Tavares",
+				"assistencias": 4
+			},
+			{
+				"nome": "Lucas Pratto",
+				"assistencias": 2
+			},
+			{
+				"nome": "Cueva",
+				"assistencias": 2
+			},
+			{
+				"nome": "Marcinho",
+				"assistencias": 2
+			},
+			{
+				"nome": "Petros",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 277,
+		"nome": "Santos",
+		"abreviacao": "SAN",
+		"posicao": 3,
+		"escudos": {
+			"grande": "img/escudos/santos_60x60.png",
+			"medio": "img/escudos/santos_45x45.png",
+			"pequeno": "img/escudos/santos_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Copete",
+				"gols": 5
+			},
+			{
+				"nome": "Bruno Henrique",
+				"gols": 4
+			},
+			{
+				"nome": "Kayke",
+				"gols": 3
+			},
+			{
+				"nome": "David Braz",
+				"gols": 2
+			},
+			{
+				"nome": "Victor Ferraz",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Bruno Henrique",
+				"assistencias": 6
+			},
+			{
+				"nome": "Lucas Lima",
+				"assistencias": 3
+			},
+			{
+				"nome": "Jean Mota",
+				"assistencias": 2
+			},
+			{
+				"nome": "Kayke",
+				"assistencias": 1
+			},
+			{
+				"nome": "Daniel Guedes ",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 282,
+		"nome": "Atlético-MG",
+		"abreviacao": "ATL",
+		"posicao": 10,
+		"escudos": {
+			"grande": "img/escudos/atletico_mg_60x60.png",
+			"medio": "img/escudos/atletico_mg_45x45.png",
+			"pequeno": "img/escudos/atletico_mg_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Fred",
+				"gols": 5
+			},
+			{
+				"nome": "Rafael Moura",
+				"gols": 3
+			},
+			{
+				"nome": "Marlone",
+				"gols": 2
+			},
+			{
+				"nome": "Elias",
+				"gols": 2
+			},
+			{
+				"nome": "Cazares",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Cazares",
+				"assistencias": 5
+			},
+			{
+				"nome": "Alex Silva",
+				"assistencias": 3
+			},
+			{
+				"nome": "Marcos Rocha",
+				"assistencias": 2
+			},
+			{
+				"nome": "Robinho",
+				"assistencias": 2
+			},
+			{
+				"nome": "Valdívia",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 283,
+		"nome": "Cruzeiro",
+		"abreviacao": "CRU",
+		"posicao": 9,
+		"escudos": {
+			"grande": "img/escudos/cruzeiro_65.png",
+			"medio": "img/escudos/cruzeiro_45.png",
+			"pequeno": "img/escudos/cruzeiro_30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Thiago Neves",
+				"gols": 6
+			},
+			{
+				"nome": "Sassá",
+				"gols": 3
+			},
+			{
+				"nome": "Rafael Sobis",
+				"gols": 2
+			},
+			{
+				"nome": "Robinho",
+				"gols": 2
+			},
+			{
+				"nome": "Élber",
+				"gols": 1
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Alisson",
+				"assistencias": 5
+			},
+			{
+				"nome": "Diogo Barbosa",
+				"assistencias": 2
+			},
+			{
+				"nome": "Lucas Romero",
+				"assistencias": 2
+			},
+			{
+				"nome": "Thiago Neves",
+				"assistencias": 2
+			},
+			{
+				"nome": "Rafael Sobis",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 284,
+		"nome": "Grêmio",
+		"abreviacao": "GRE",
+		"posicao": 2,
+		"escudos": {
+			"grande": "img/escudos/gremio_60x60.png",
+			"medio": "img/escudos/gremio_45x45.png",
+			"pequeno": "img/escudos/gremio_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Fernandinho",
+				"gols": 6
+			},
+			{
+				"nome": "Luan",
+				"gols": 5
+			},
+			{
+				"nome": "Everton",
+				"gols": 5
+			},
+			{
+				"nome": "Michel ",
+				"gols": 4
+			},
+			{
+				"nome": "Ramiro",
+				"gols": 4
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Luan",
+				"assistencias": 6
+			},
+			{
+				"nome": "Ramiro",
+				"assistencias": 3
+			},
+			{
+				"nome": "Pedro Rocha",
+				"assistencias": 2
+			},
+			{
+				"nome": "Pedro Geromel",
+				"assistencias": 2
+			},
+			{
+				"nome": "Everton",
+				"assistencias": 2
+			}
+		]
+	},
+	{
+		"id": 287,
+		"nome": "Vitória",
+		"abreviacao": "VIT",
+		"posicao": 19,
+		"escudos": {
+			"grande": "img/escudos/vitoria_60x60.png",
+			"medio": "img/escudos/vitoria_45x45.png",
+			"pequeno": "img/escudos/vitoria_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Neilton",
+				"gols": 4
+			},
+			{
+				"nome": "Uillian Correia",
+				"gols": 3
+			},
+			{
+				"nome": "Kieza",
+				"gols": 3
+			},
+			{
+				"nome": "David",
+				"gols": 2
+			},
+			{
+				"nome": "André Lima",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "David",
+				"assistencias": 4
+			},
+			{
+				"nome": "Neilton",
+				"assistencias": 3
+			},
+			{
+				"nome": "Cleiton Xavier",
+				"assistencias": 3
+			},
+			{
+				"nome": "André Lima",
+				"assistencias": 2
+			},
+			{
+				"nome": "Patric",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 292,
+		"nome": "Sport",
+		"abreviacao": "SPO",
+		"posicao": 6,
+		"escudos": {
+			"grande": "img/escudos/sport65.png",
+			"medio": "img/escudos/sport45.png",
+			"pequeno": "img/escudos/sport30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "André",
+				"gols": 9
+			},
+			{
+				"nome": "Diego Souza",
+				"gols": 6
+			},
+			{
+				"nome": "Osvaldo",
+				"gols": 3
+			},
+			{
+				"nome": "Patrick",
+				"gols": 2
+			},
+			{
+				"nome": "Mena",
+				"gols": 1
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Mena",
+				"assistencias": 4
+			},
+			{
+				"nome": "Lenis",
+				"assistencias": 3
+			},
+			{
+				"nome": "Rithely",
+				"assistencias": 2
+			},
+			{
+				"nome": "Marquinhos",
+				"assistencias": 2
+			},
+			{
+				"nome": "Everton Felipe",
+				"assistencias": 2
+			}
+		]
+	},
+	{
+		"id": 293,
+		"nome": "Atlético-PR",
+		"abreviacao": "ATL",
+		"posicao": 15,
+		"escudos": {
+			"grande": "img/escudos/atletico-pr_2015_65.png",
+			"medio": "img/escudos/atletico-pr_2015_45.png",
+			"pequeno": "img/escudos/atletico-pr_2015_30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Sidcley",
+				"gols": 3
+			},
+			{
+				"nome": "Guilherme",
+				"gols": 3
+			},
+			{
+				"nome": "Eduardo Henrique",
+				"gols": 2
+			},
+			{
+				"nome": "Thiago Heleno",
+				"gols": 2
+			},
+			{
+				"nome": "Wanderson",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Sidcley",
+				"assistencias": 3
+			},
+			{
+				"nome": "Nikão",
+				"assistencias": 2
+			},
+			{
+				"nome": "Guilherme",
+				"assistencias": 2
+			},
+			{
+				"nome": "Jonathan",
+				"assistencias": 1
+			},
+			{
+				"nome": "Éderson",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 294,
+		"nome": "Coritiba",
+		"abreviacao": "COR",
+		"posicao": 17,
+		"escudos": {
+			"grande": "img/escudos/coritiba65.png",
+			"medio": "img/escudos/coritiba45.png",
+			"pequeno": "img/escudos/coritiba30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Henrique Almeida",
+				"gols": 5
+			},
+			{
+				"nome": "Kleber",
+				"gols": 4
+			},
+			{
+				"nome": "Rildo",
+				"gols": 4
+			},
+			{
+				"nome": "Tomas Bastos",
+				"gols": 2
+			},
+			{
+				"nome": "Thiago Carleto",
+				"gols": 1
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Neto Berola",
+				"assistencias": 2
+			},
+			{
+				"nome": "Rildo",
+				"assistencias": 2
+			},
+			{
+				"nome": "Tiago Real",
+				"assistencias": 2
+			},
+			{
+				"nome": "Kleber",
+				"assistencias": 1
+			},
+			{
+				"nome": "Henrique Almeida",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 303,
+		"nome": "Ponte Preta",
+		"abreviacao": "PON",
+		"posicao": 13,
+		"escudos": {
+			"grande": "img/escudos/ponte_preta_60x60.png",
+			"medio": "img/escudos/ponte_preta_45x45.png",
+			"pequeno": "img/escudos/ponte_preta_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Lucca",
+				"gols": 10
+			},
+			{
+				"nome": "Emerson Sheik",
+				"gols": 2
+			},
+			{
+				"nome": "Nino Paraíba",
+				"gols": 1
+			},
+			{
+				"nome": "Léo Artur",
+				"gols": 1
+			},
+			{
+				"nome": "Naldo",
+				"gols": 1
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Nino Paraíba",
+				"assistencias": 2
+			},
+			{
+				"nome": "Renato Cajá",
+				"assistencias": 2
+			},
+			{
+				"nome": "Ravanelli",
+				"assistencias": 1
+			},
+			{
+				"nome": "Lucca",
+				"assistencias": 1
+			},
+			{
+				"nome": "Léo Artur",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 314,
+		"nome": "Avaí",
+		"abreviacao": "AVA",
+		"posicao": 18,
+		"escudos": {
+			"grande": "img/escudos/avai_60x60_.png",
+			"medio": "img/escudos/avai_45x45.png",
+			"pequeno": "img/escudos/avai_30x30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Júnior Dutra",
+				"gols": 3
+			},
+			{
+				"nome": "Romulo",
+				"gols": 2
+			},
+			{
+				"nome": "Joel",
+				"gols": 2
+			},
+			{
+				"nome": "Juan",
+				"gols": 1
+			},
+			{
+				"nome": "Wellington Simião",
+				"gols": 1
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Leandro Silva",
+				"assistencias": 2
+			},
+			{
+				"nome": "Willians",
+				"assistencias": 1
+			},
+			{
+				"nome": "Júnior Dutra",
+				"assistencias": 1
+			},
+			{
+				"nome": "Judson",
+				"assistencias": 1
+			},
+			{
+				"nome": "Capa",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 315,
+		"nome": "Chapecoense",
+		"abreviacao": "CHA",
+		"posicao": 11,
+		"escudos": {
+			"grande": "img/escudos/Escudo-Chape-165.png",
+			"medio": "img/escudos/Escudo-Chape-145.png",
+			"pequeno": "img/escudos/Escudo-Chape-130.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Arthur ",
+				"gols": 4
+			},
+			{
+				"nome": "Wellington Paulista",
+				"gols": 3
+			},
+			{
+				"nome": "Reinaldo",
+				"gols": 3
+			},
+			{
+				"nome": "Luiz Antonio ",
+				"gols": 2
+			},
+			{
+				"nome": "Túlio de Melo",
+				"gols": 2
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Arthur ",
+				"assistencias": 5
+			},
+			{
+				"nome": "Seijas",
+				"assistencias": 2
+			},
+			{
+				"nome": "Andrei Girotto",
+				"assistencias": 2
+			},
+			{
+				"nome": "Luiz Otávio ",
+				"assistencias": 1
+			},
+			{
+				"nome": "Reinaldo",
+				"assistencias": 1
+			}
+		]
+	},
+	{
+		"id": 373,
+		"nome": "Atlético-GO",
+		"abreviacao": "ATL",
+		"posicao": 20,
+		"escudos": {
+			"grande": "img/escudos/Atletico-GO-65.png",
+			"medio": "img/escudos/Atletico-GO-45.png",
+			"pequeno": "img/escudos/Atletico-GO-30.png"
+		},
+		"artilheiros": [
+			{
+				"nome": "Andrigo",
+				"gols": 2
+			},
+			{
+				"nome": "Paulinho",
+				"gols": 2
+			},
+			{
+				"nome": "Diego Rosa",
+				"gols": 1
+			},
+			{
+				"nome": "Jorginho",
+				"gols": 1
+			},
+			{
+				"nome": "Luiz Fernando ",
+				"gols": 1
+			}
+		],
+		"lideresassistencia": [
+			{
+				"nome": "Diego Rosa",
+				"assistencias": 2
+			},
+			{
+				"nome": "Andrigo",
+				"assistencias": 2
+			},
+			{
+				"nome": "Bruno Pacheco",
+				"assistencias": 2
+			},
+			{
+				"nome": "Roger Carvalho",
+				"assistencias": 1
+			},
+			{
+				"nome": "Luiz Fernando ",
+				"assistencias": 1
+			}
+		]
+	}
+];
+}
